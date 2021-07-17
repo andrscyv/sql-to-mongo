2e59e36d9e35ec7835d464b424cea5dc011bd868
Andres Cruz <andrscyv@gmail.com>
2021-07-17 18:50:45 -0500

    Hardcoded example working

--- src/exportDefinition.ts
+++ src/exportDefinition.ts
@@ -12,19 +12,19 @@ export class ExportDefinition {
 
     async run(): Promise<void> {
         const mongo = createMongoClient();
-        // await mongo.connect()
+        await mongo.connect()
 
         try {
             await sql.unsafe(this.sqlQuery).cursor(2000, async (rows: any) => {
                 if (this.beforeInsert) { await this.beforeInsert(rows); }
                 console.log(rows)
-                // await mongo.db(process.env.MONGO_DB_NAME).collection(this.collectionName).insertMany(rows);
+                await mongo.db(process.env.MONGO_DB_NAME).collection(this.collectionName).insertMany(rows);
                 if (this.afterInsert) { await this.afterInsert(); }
             }) 
         } catch (error) {
             throw error;
         } finally {
-            // await mongo.close();
+            await mongo.close();
         }
 
     }
